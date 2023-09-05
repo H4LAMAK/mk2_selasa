1f8ec152c705b159fb98f1562cf2920fb7edfe72
Kallatlat <126229439+Kallatlat@users.noreply.github.com>
2023-09-05 15:51:12 +0800

    SIAP
    SIAP

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head><title>Text Processing</title>
+</head>
+<body>
+<h1>Malaysia Madani</h1>
+<form action="papar.php" method="post">
+    <label for="input_text">Cerita tentang diri anda:</label><br> <textarea id="input_text" name="input_text" rows="4"
+                                                                            cols="50"></textarea>
+    <br> <input type="submit" value="Submit">
+</form>
+</body>
+</html>
